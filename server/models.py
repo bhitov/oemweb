ec38a9ede81022a100de2a5e20cfd3bf8b7b31d6
Benjamin Hitov <Benjamin.Hitov@gmail.com>
2011-12-04 13:26:05 -0500

    second commit!

--- server/models.py
+++ server/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+class Post(models.Model):
+    data = models.CharField(max_length = 150, blank=True, null=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    
+class KML(models.Model):
+    name = models.CharField(max_length = 100)
+    file = models.FileField(upload_to="files")
--- server/views.py
+++ server/views.py
@@ -1,1 +1,95 @@
 # Create your views here.
+from django.shortcuts import render, render_to_response, get_object_or_404, redirect
+from django.http import HttpResponseRedirect, HttpResponse
+from django.utils import simplejson
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_exempt
+from django.views.generic import list_detail, create_update
+from server.models import Post, KML
+
+
+
+
+def KML_detail(request, title):
+    print "in KML_detail with title: %s" % title
+    kml = KML.objects.get(name=title)
+    
+    return list_detail.object_list(
+                    request,
+                    queryset=KML.objects.filter(name=title),
+                    template_name='server/kml_list.html',
+                    template_object_name="KML"
+                    )
+@csrf_exempt
+def KML_form(request):
+    if request.POST:
+        
+        pass
+    print 'in kml form'
+    return create_update.create_object(
+                    request,
+                    model=KML,
+                    post_save_redirect='/KML')
+    
+
+def post_list(request):
+    print 'in post_list'
+    return list_detail.object_list(
+                    request,
+                    queryset=Post.objects.all(),
+                    template_object_name='post')
+
+@csrf_exempt
+def ep_post(request):
+    print 'in ep_post'
+    if request.POST:
+        print 'POST detected'
+        print request.POST
+        print 'starting parse'
+    return HttpResponse('1')
+
+@csrf_exempt
+def post_create_ajax(request):
+    print 'in ajax view'
+    print request.POST
+    text = request.POST['data']
+    #print request.POST
+    response = {'status' : 'E'}
+    try:
+        print 'got text %s' % text
+        post = Post(data=text)
+        post.save()
+        print 'got here'
+        response['status'] = 'S'
+        print 'and here'
+    except:
+        pass
+    json = simplejson.dumps(response)
+    return HttpResponse(json, mimetype='application/json')
+
+@csrf_exempt
+def post_form(request):
+    print 'in post_form'
+    if request.is_ajax():
+        print 'in post_form ajax'
+        text = request.POST['data']
+        response = {'status' : 'E'}
+        try:
+            print 'got text %s' % text
+            post = Post(data=text)
+            post.save()
+            print 'got here'
+            response['status'] = 'S'
+            print 'and here'
+        except:
+            pass
+        json = simplejson.dumps(response)
+        return HttpResponse(json, mimetype='application/json')
+        pass
+    
+    print 'in post_form'
+    return create_update.create_object(
+                    request,
+                    model=Post,
+                    post_save_redirect='/posts')
+    
