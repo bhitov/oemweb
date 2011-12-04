2ae210fe0aa020236c9c839ebad957d99cec4bb9
Avni Khatri <avni@Avnis-MacBook-Air.local>
2011-12-04 15:16:28 -0500

    Adding Hazards class

--- server/models.py
+++ server/models.py
@@ -1,6 +1,19 @@
 from django.db import models
 from datetime import datetime
 
+class Hazards(models.Model):
+    submitted_time = models.DateTimeField(auto_now=True)
+    submitted_username = models.CharField(max_length = 150)
+    source = models.CharField(max_length = 150, blank=True, null=True)
+    hazard_type = models.CharField(max_length = 150, blank=True, null=True)
+    hazard_subfield_one = models.CharField(max_length = 150, blank=True, null=True)
+    hazard_subfield_two = models.CharField(max_length = 150, blank=True, null=True)
+    additional_info = models.TextField(blank=True, null=True)
+    ticket_latitude = models.FloatField(blank=True, null=True)
+    ticket_longitude = models.FloatField(blank=True, null=True)
+    cleared_time = models.DateTimeField(blank=True, null=True)
+    cleared_username = models.CharField(max_length = 150)
+
 class Post(models.Model):
     data = models.CharField(max_length = 150, blank=True, null=True)
     timestamp = models.DateTimeField(auto_now_add=True)
@@ -8,3 +21,5 @@ class Post(models.Model):
 class KML(models.Model):
     name = models.CharField(max_length = 100)
     file = models.FileField(upload_to="files")
+    filename = models.CharField(max_length=300)
+#    filenamex = models.FileField(upload_to="files").name
